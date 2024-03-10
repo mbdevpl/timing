3c1b5d0da61996098ec8c2d9406f7d68a798b622
Mateusz Bysiek <mateusz.bysiek@gmail.com>
2024-03-10 19:41:35 +0900

    refactor: make Timing more strict

--- test/test_timing.py
+++ test/test_timing.py
@@ -1,32 +1,69 @@
 """Tests for the time measurement itself."""
 
+import copy
 import logging
 import time
 import unittest
 
-from timing.timing import Timing
+from timing.timing import Timing, TimingState
 
 _LOG = logging.getLogger(__name__)
 
 
 class Tests(unittest.TestCase):
 
-    def test_basic(self):
+    def test_name(self):
         timer = Timing('timing')
-        self.assertNotEqual(timer, 'timing')
-        other_timer = Timing('timing')
-        self.assertEqual(timer, other_timer)
         self.assertEqual(timer.name, 'timing')
-        self.assertIsNone(timer.begin)
-        self.assertIsNone(timer.end)
-        self.assertIsNone(timer.elapsed)
+        self.assertEqual(timer.state, TimingState.NOT_STARTED)
+        with self.assertRaises(AssertionError):
+            _LOG.debug('%f', timer.begin)
+        with self.assertRaises(AssertionError):
+            _LOG.debug('%f', timer.end)
+        with self.assertRaises(AssertionError):
+            _LOG.debug('%f', timer.elapsed)
+
+    def test_start(self):
+        timer = Timing('timing')
+        timer.start()
+        self.assertEqual(timer.state, TimingState.RUNNING)
+        time.sleep(0.01)
+        self.assertIsInstance(timer.begin, float)
+        self.assertGreater(timer.begin, 0)
+        with self.assertRaises(AssertionError):
+            _LOG.debug('%f', timer.end)
+        with self.assertRaises(AssertionError):
+            _LOG.debug('%f', timer.elapsed)
+
+    def test_stop(self):
+        timer = Timing('timing')
         timer.start()
         time.sleep(0.01)
-        self.assertIsNotNone(timer.begin)
         timer.stop()
-        self.assertIsNotNone(timer.end)
+        self.assertEqual(timer.state, TimingState.FINISHED)
+        self.assertIsInstance(timer.end, float)
+        self.assertGreater(timer.end, 0)
+        self.assertIsInstance(timer.elapsed, float)
         self.assertGreater(timer.elapsed, 0)
-        _LOG.info('%s', timer)
-        _LOG.info('%r', timer)
-        self.assertEqual(timer, timer)
+
+    def test_compare(self):
+        timer = Timing('timing')
+        other_timer = Timing('timing')
+        self.assertEqual(timer, other_timer)
+        # start
+        timer.start()
         self.assertNotEqual(timer, other_timer)
+        other_timer.start()
+        self.assertEqual(timer.state, other_timer.state)
+        self.assertNotEqual(timer, other_timer)
+        other_timer = copy.deepcopy(timer)
+        time.sleep(0.01)
+        self.assertEqual(timer, other_timer)
+        # stop
+        timer.stop()
+        self.assertNotEqual(timer, other_timer)
+        other_timer.stop()
+        self.assertEqual(timer.state, other_timer.state)
+        self.assertNotEqual(timer, other_timer)
+        other_timer = copy.deepcopy(timer)
+        self.assertEqual(timer, other_timer)
--- timing/timing.py
+++ timing/timing.py
@@ -1,7 +1,17 @@
 """Core module of the timing package."""
 
+import enum
 import time
 import typing as t
+
+
+@enum.unique
+class TimingState(enum.IntEnum):
+    """State of a timing."""
+
+    NOT_STARTED = 0
+    RUNNING = 1
+    FINISHED = 2
 
 
 class Timing:
@@ -13,17 +23,16 @@ class Timing:
     def __init__(self, name: str):
         assert isinstance(name, str), type(name)
         assert name
-
         # self._group = None  # type: t.Optional[TimingGroup]
         self._name: str = name
+        self._state: int = 0
         self._begin: t.Optional[float] = None
         self._end: t.Optional[float] = None
         self._elapsed: t.Optional[float] = None
 
     def _calculate_elapsed(self) -> None:
-        assert isinstance(self._begin, float)
-        assert isinstance(self._end, float)
-
+        assert self._begin is not None, 'timing has not started yet'
+        assert self._end is not None, 'timing has not finished yet'
         self._elapsed = self._end - self._begin
 
     @property
@@ -31,31 +40,55 @@ class Timing:
         return self._name
 
     @property
-    def begin(self) -> t.Optional[float]:
+    def begin(self) -> float:
+        assert self._begin is not None, 'timing has not started yet'
         return self._begin
 
     @property
-    def end(self) -> t.Optional[float]:
+    def end(self) -> float:
+        assert self._end is not None, 'timing has not finished yet'
         return self._end
 
     @property
-    def elapsed(self) -> t.Optional[float]:
+    def elapsed(self) -> float:
+        assert self._elapsed is not None, 'timing has not finished yet'
         return self._elapsed
+
+    @property
+    def state(self) -> TimingState:
+        return {
+            0: TimingState.NOT_STARTED,
+            1: TimingState.RUNNING,
+            2: TimingState.FINISHED,
+        }[self._state]
 
     def start(self) -> None:
         """Start the timer."""
+        self._state = 1
+        self._end = None
+        self._elapsed = None
         self._begin = time.perf_counter()
 
     def stop(self) -> None:
         """Stop the timer."""
         self._end = time.perf_counter()
+        if self.state is not TimingState.RUNNING:
+            self._end = None
+        assert self.state is TimingState.RUNNING, 'timing has not started yet'
+        self._state = 2
         self._calculate_elapsed()
 
     def __eq__(self, other):
-        if not isinstance(other, Timing):
+        if not isinstance(other, Timing) or self.state is not other.state \
+                or self._name != other.name:
             return False
-        return self._name == other.name and self._begin == other.begin \
-            and self._end == other.end and self._elapsed == other.elapsed
+        if self._state == 0:
+            return True
+        if self._begin != other.begin:
+            return False
+        if self.state == 1:
+            return True
+        return self._end == other.end
 
     def __str__(self):
         args = [self._name, self._begin, self._end, self._elapsed]
